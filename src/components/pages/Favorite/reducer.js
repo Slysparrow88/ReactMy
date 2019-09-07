ec3cd01a55336e6b3eb894c58bb6386e4bb41090
maya <shkoolnik@gmail.com>
2019-09-07 18:41:05 +0300

    second

---
+++ src/components/pages/Favorite/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    name: 'FAVORITE',
+    posts: [],
+}
+
+export default function (state = initialState, action){
+
+    if(action.type === 'SET_POSTS'){
+        return {
+            ...state,
+            posts:action.dataPosts
+        };
+    }  
+    return state;
+}
